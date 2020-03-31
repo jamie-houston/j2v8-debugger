ff88b8ed09a1af6a23f153edea124642f910f22f
Jamie Houston <jamie.houston@salesforce.com>
2020-03-31 13:09:45 -0500

    fixed tests

--- j2v8-debugger/src/test/java/com/alexii/j2v8debugger/StethoHelperTest.kt
+++ j2v8-debugger/src/test/java/com/alexii/j2v8debugger/StethoHelperTest.kt
@@ -1,8 +1,10 @@
 package com.alexii.j2v8debugger
 
 import android.app.Application
-import com.eclipsesource.v8.debug.DebugHandler
-import com.nhaarman.mockito_kotlin.*
+import com.eclipsesource.v8.inspector.V8Inspector
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -19,64 +21,69 @@ class StethoHelperTest {
 
     @Test
     fun `returns custom Debugger and no Stetho Debugger Stub`() {
-        val scriptSourceProviderMock = mock<ScriptSourceProvider> {}
-        val contextMock = mock<Application> {}
-        whenever(contextMock.applicationContext).thenReturn(contextMock)
+        val scriptSourceProviderMock = mockk<ScriptSourceProvider> {}
+        val contextMock = mockk<Application> {}
+        every {
+            contextMock.applicationContext
+        } returns contextMock
 
         val domains = StethoHelper.getDefaultInspectorModulesWithDebugger(contextMock, scriptSourceProviderMock)
 
         assertTrue("No Debugger present", domains.any { it.javaClass == Debugger::class.java })
-        assertFalse("Stetho Debugger present", domains.any { it.javaClass ==  FacebookDebuggerStub::class.java} )
+        assertFalse("Stetho Debugger present", domains.any { it.javaClass == FacebookDebuggerStub::class.java })
     }
 
     @Test
     fun `returns custom Runtime and no Stetho base Runtime`() {
-        val scriptSourceProviderMock = mock<ScriptSourceProvider> {}
-        val contextMock = mock<Application> {}
-        whenever(contextMock.applicationContext).thenReturn(contextMock)
+        val scriptSourceProviderMock = mockk<ScriptSourceProvider> {}
+        val contextMock = mockk<Application> {}
+        every { contextMock.applicationContext } returns contextMock
 
         val domains = StethoHelper.getDefaultInspectorModulesWithDebugger(contextMock, scriptSourceProviderMock)
 
         assertTrue("No Debugger present", domains.any { it.javaClass == Runtime::class.java })
-        assertFalse("Stetho Debugger present", domains.any { it.javaClass ==  FacebookRuntimeBase::class.java} )
+        assertFalse("Stetho Debugger present", domains.any { it.javaClass == FacebookRuntimeBase::class.java })
     }
 
     @Test
     fun `initialized when Stetho created before v8`() {
-        val scriptSourceProviderMock = mock<ScriptSourceProvider> {}
-        val contextMock = mock<Application> {}
-        whenever(contextMock.applicationContext).thenReturn(contextMock)
+        val scriptSourceProviderMock = mockk<ScriptSourceProvider> {}
+        val contextMock = mockk<Application> {}
+        every { contextMock.applicationContext } returns contextMock
         StethoHelper.getDefaultInspectorModulesWithDebugger(contextMock, scriptSourceProviderMock)
 
-        val v8DebugHandlerMock = mock<DebugHandler>()
-        val v8ExecutorServiceMock = mock<ExecutorService> {
-            on { execute(any()) } doAnswer { it.getArgument<Runnable>(0).run() }
+        val v8InspectorMock = mockk<V8Inspector>()
+        val v8ExecutorServiceMock = mockk<ExecutorService> {
+            every {
+                execute(any())
+            } answers { arg<Runnable>(0).run() }
         }
-        StethoHelper.initializeWithV8Debugger(v8DebugHandlerMock, v8ExecutorServiceMock)
+        StethoHelper.initializeWithV8Debugger(v8InspectorMock, v8ExecutorServiceMock)
 
-        verify(v8ExecutorServiceMock, times(1)).execute(any())
+        verify(exactly = 1) { v8ExecutorServiceMock.execute(any()) }
         assertTrue(StethoHelper.isStethoAndV8DebuggerFullyInitialized)
     }
 
     //xxx: check why test is failing if run together with other, but ok when run separately
     @Test
     fun `initialized when v8 created before Stetho`() {
-        val v8DebugHandlerMock = mock<DebugHandler>()
-        val v8ExecutorServiceMock = mock<ExecutorService> {
-            on { execute(any()) } doAnswer { it.getArgument<Runnable>(0).run() }
+        val v8InspectorMock = mockk<V8Inspector>()
+        val v8ExecutorServiceMock = mockk<ExecutorService> {
+            every { execute(any()) } answers { arg<Runnable>(0).run() }
         }
-        StethoHelper.initializeWithV8Debugger(v8DebugHandlerMock, v8ExecutorServiceMock)
+        StethoHelper.initializeWithV8Debugger(v8InspectorMock, v8ExecutorServiceMock)
 
-        val scriptSourceProviderMock = mock<ScriptSourceProvider> {}
-        val contextMock = mock<Application> {}
-        whenever(contextMock.applicationContext).thenReturn(contextMock)
+        val scriptSourceProviderMock = mockk<ScriptSourceProvider> {}
+        val contextMock = mockk<Application> {}
+        every { contextMock.applicationContext } returns contextMock
+
         StethoHelper.getDefaultInspectorModulesWithDebugger(contextMock, scriptSourceProviderMock)
 
-        verify(v8ExecutorServiceMock, times(1)).execute(any())
+        verify(exactly = 1) { v8ExecutorServiceMock.execute(any()) }
         assertTrue(StethoHelper.isStethoAndV8DebuggerFullyInitialized)
     }
 
 
     val StethoHelper.isStethoAndV8DebuggerFullyInitialized
-        get() = this.debugger?.v8Debugger != null
+        get() = this.debugger?.v8Inspector != null
 }
